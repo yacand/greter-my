821aa6b5d2b612f22376b987d1eaa9a13aa72f4f
Andrey <yacand@gmail.com>
2017-01-28 12:57:34 +0300

    [CHG]

--- src/ts/version.ts
+++ src/ts/version.ts
@@ -3,6 +3,6 @@ class Version
 	public VERSION:number = 1; //-- Public launch 
 	public MAJOR:number = 0; //-- Milestone 
 	public MINOR:number = 0; //-- Sprints / intermediate releases 
-	public REVISION:number = 119; //-- build increment 
-	public DATE:string = "28.01.2017 12:48:47,86" 
+	public REVISION:number = 101; //-- build increment 
+	public DATE:string = "28.01.2017 12:53:05,84" 
 } 
